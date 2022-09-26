654c18b242c6a29c32b3b18881ac9447515900f4
DeepaliKosta <deepalikosta1234@gmail.com>
2022-09-26 14:05:03 +0530

    Minor changes in Customer module
    In the customer service impl nd empty customer list exception

--- src/main/java/com/onlinesweetmart/exception/emptyCustomerListException.java
+++ src/main/java/com/onlinesweetmart/exception/emptyCustomerListException.java
@@ -1,10 +1,12 @@
 package com.onlinesweetmart.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor @Data
+@AllArgsConstructor
+@Data
 public class EmptyCustomerListException extends Exception{
 	private static final long serialVersionUID = 1L;
 	String msg;
