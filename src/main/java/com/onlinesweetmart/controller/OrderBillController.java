dbd11cc8c8191366ade1eb8e35a9a9baf022e200
Sudeep-Ghosh <70695745+Sudeep-Ghosh@users.noreply.github.com>
2022-09-27 16:32:40 +0530

    Update OrderBillController.java

--- src/main/java/com/onlinesweetmart/controller/OrderBillController.java
+++ src/main/java/com/onlinesweetmart/controller/OrderBillController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class OrderBillController {
 		return new ResponseEntity<OrderBill>(orderBill2, HttpStatus.OK);
 	}
 	
-	@PostMapping(value="orderBill")
+	@PutMapping(value="orderBill")
 	ResponseEntity<OrderBill> updateOrderBill(@RequestBody OrderBill orderBill){
 		
 		OrderBill orderBill3 = orderBillService.updateOrderBill(orderBill);
